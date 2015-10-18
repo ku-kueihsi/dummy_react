38fc8a00fe4dfa7d2e47da48fe98633e372753e8
Ku, Kuei-Hsi <ku.kueihsi@gmail.com>
2015-10-18 09:54:22 -0700

    Update express server. Better log and hello world.

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,15 +1,23 @@
 /// <reference path='../../typings/express/express.d.ts' />
 import express = require('express');
+
+var STATIC_PATH = '/static';
+var PORT = 1949;
+
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send(
+    'Example app at: ' + 'localhost:' + PORT + STATIC_PATH + '/index.html');
 });
 
-app.use(express.static('public'));
-var server = app.listen(1949, function () {
+app.use(STATIC_PATH, express.static(__dirname + '/../public'));
+// app.use(express.static('public'));
+var server = app.listen(PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
-
+  
   console.log('Example app listening at http://%s:%s', host, port);
+  console.log(
+    'Example app at: ' + 'localhost:' + port + STATIC_PATH + '/index.html');
 });
