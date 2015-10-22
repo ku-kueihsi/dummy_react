c855fbe5b6242914e882b71b5bc02fc3b261c675
Ku, Kuei-Hsi <ku.kueihsi@gmail.com>
2015-10-21 18:35:12 -0700

    Add sourcemap and minify.

--- public/src/webpack.config.js
+++ public/src/webpack.config.js
@@ -1,12 +1,21 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: './hello_react_typescript.tsx',
   output: {
+    path: __dirname + "/../scripts",
     filename: 'bundle.js'
   },
+  // Turn on sourcemaps
+  devtool: 'source-map',
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
+  // Add minification
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ],
   module: {
     loaders: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
